be74e0faf8232465d3c144a1ca26b908bfdeb3b0
osman-gif <osmanngong@gmail.com>
2023-12-17 15:10:47 +0200

    Modified the 2-selection_sort.c file

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -30,8 +30,8 @@ void selection_sort(int *array, size_t size)
 			tmp = array[i];
 			array[i] = array[small];
 			array[small] = tmp;
-		}
 
-		print_array(array, size);
+			print_array(array, size);
+		}
 	}
 }
