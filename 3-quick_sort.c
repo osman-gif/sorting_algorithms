965a8a6d70c761865cb9c6796a8e797385ef6659
osman-gif <osmanngong@gmail.com>
2023-12-18 18:16:48 +0200

    Added 3-quick_sort.c and 3-O files

---
+++ 3-quick_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * quick_sort - Sort an array of ints using quick sort
+ * @array: Array to be sorted
+ * @size: Size of the array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+}
